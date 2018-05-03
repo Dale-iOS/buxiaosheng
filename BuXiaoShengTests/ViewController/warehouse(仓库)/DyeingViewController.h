7053c93fe1a6bd7e6c499d450f3809de6d6060d2
周尊贤 <lanyuzx@qq.com>
2018-05-03 10:52:09 +0800

    增加XLFORM

--- BuXiaoShengTests/ViewController/warehouse(仓库)/DyeingViewController.h
+++ BuXiaoShengTests/ViewController/warehouse(仓库)/DyeingViewController.h
@@ -7,8 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "BaseViewController.h"
+#import "XLFormViewController.h"
 
-@interface DyeingViewController : BaseViewController
+@interface DyeingViewController : XLFormViewController
 
 @end
