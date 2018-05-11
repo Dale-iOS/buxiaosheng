230386e069bcb006e1b76e50c5d3e075bf459cf1
周尊贤 <lanyuzx@qq.com>
2018-05-11 12:37:34 +0800

    现金银行

---
+++ BuXiaoShengTests/Model/Setting设置/LLCashBankModel.h
@@ -0,0 +1,18 @@
+//
+//  LLCashBankModel.h
+//  BuXiaoSheng
+//
+//  Created by 周尊贤 on 2018/5/11.
+//  Copyright © 2018年 BuXiaoSheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLCashBankModel : NSObject
+@property (nonatomic,copy) NSString * cardNumber;
+@property (nonatomic,copy) NSString * id;
+@property (nonatomic,copy) NSString * name;
+@property (nonatomic,copy) NSString * status;
+@property (nonatomic,copy) NSString * systemItem;
+
+@end
