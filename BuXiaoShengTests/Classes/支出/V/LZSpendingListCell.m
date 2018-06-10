1decdef212b31a1cc1bef0243f97172596cc3ea1
罗镇浩 <diyihuomao@163.com>
2018-06-10 20:56:29 +0800

    LZSpendingListCell.m 审批还没开始

--- BuXiaoShengTests/Classes/支出/V/LZSpendingListCell.m
+++ BuXiaoShengTests/Classes/支出/V/LZSpendingListCell.m
@@ -14,8 +14,10 @@
     UILabel *_bankPayLbl;//银行方式
     UILabel *_dateLbl;//日期
     UILabel *_money;//费用
-    UIImageView *_auditIM;//审批图标
-    UILabel *_auditLbl;//审批状态
+    UIImageView *_auditYesIM;//审批通过图标
+    UIImageView *_auditNoIM;//审批拒绝图标
+    UILabel *_auditYesLbl;//审批通过状态
+    UILabel *_auditNoLbl;//审批拒绝状态
 }
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
@@ -50,6 +52,33 @@
         make.width.mas_offset(100);
         make.height.mas_offset(14);
     }];
+    
+    //日期
+    _dateLbl = [[UILabel alloc]init];
+    _dateLbl.textColor = CD_Text66;
+    _dateLbl.font = FONT(13);
+    [self.contentView addSubview:_dateLbl];
+    [_dateLbl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView).offset(15);
+        make.top.equalTo(_bankPayLbl).offset(10);
+        make.height.mas_offset(14);
+        make.width.mas_offset(100);
+    }];
+    
+    //费用
+    _money = [[UILabel alloc]init];
+    _money.font = FONT(17);
+    _money.textColor = CD_Text33;
+    [self.contentView addSubview:_money];
+    [_money mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView).offset(115);
+        make.height.mas_offset(18);
+        make.width.mas_offset(100);
+        make.centerY.equalTo(self.contentView);
+    }];
+    
+   
+    
 }
 
 - (void)awakeFromNib {
