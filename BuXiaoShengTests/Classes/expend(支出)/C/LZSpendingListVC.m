186c76a1d0e5dc8233117413cb235ff0def0c4c9
罗镇浩 <diyihuomao@163.com>
2018-06-23 11:12:37 +0800

    日常支出列表 ➕搜索和日期

--- BuXiaoShengTests/Classes/expend(支出)/C/LZSpendingListVC.m
+++ BuXiaoShengTests/Classes/expend(支出)/C/LZSpendingListVC.m
@@ -4,19 +4,33 @@
 //
 //  Created by 罗镇浩 on 2018/6/10.
 //  Copyright © 2018年 BuXiaoSheng. All rights reserved.
-//  日出支出列表页面
+//  日常支出列表页面
 
 #import "LZSpendingListVC.h"
 #import "LZSpendingListCell.h"
 #import "LZSpendingModel.h"
+#import "LLDayCalendarVc.h"
+#import "LLWeekCalendarVc.h"
+#import "LLMonthCalendarVc.h"
+#import "LLQuarterCalendarVc.h"
+#import "SGPagingView.h"
+#import "LZSearchBar.h"
 
-@interface LZSpendingListVC ()<UITableViewDelegate,UITableViewDataSource>
+@interface LZSpendingListVC ()<UITableViewDelegate,UITableViewDataSource,SGPageTitleViewDelegate,SGPageContentViewDelegate,LLDayCalendarVcDelegate,LZSearchBarDelegate>
+{
+    NSString *_startStr;//开始时间
+    NSString *_endStr;//结束时间
+}
 //顶部试图
 @property(nonatomic,strong)UIView *headView;
 @property(nonatomic,strong)UIView *rigthHeadView;
 @property(nonatomic,strong)UILabel *dateLbl;
 @property(nonatomic,strong)UITableView *tableView;
 @property(nonatomic,strong)NSArray<LZSpendingDetailModel*> *lists;
+@property(nonatomic,strong)SGPageTitleView *pageTitleView;
+@property(nonatomic,strong)SGPageContentView *pageContentView;
+@property(nonatomic,strong)UIView *bottomView;
+@property(nonatomic,strong)LZSearchBar * searchBar;
 @end
 
 @implementation LZSpendingListVC
@@ -24,6 +38,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupUI];
+    [self setupPageView];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -33,14 +48,29 @@
 }
 
 - (void)setupUI{
-    self.navigationItem.titleView = [Utility navTitleView:@"日出支出列表"];
+    self.navigationItem.titleView = [Utility navTitleView:@"日常支出列表"];
+    _startStr = @"";
+    _endStr = @"";
+    
+    //初始化搜索框
+    self.searchBar = [[LZSearchBar alloc]initWithFrame:CGRectMake(0, LLNavViewHeight, APPWidth, 49)];
+    self.searchBar.placeholder = @"输入搜索";
+    self.searchBar.textColor = Text33;
+    self.searchBar.delegate = self;
+    self.searchBar.iconImage = IMAGE(@"search1");
+    self.searchBar.backgroundColor = [UIColor whiteColor];
+    self.searchBar.placeholderColor = [UIColor colorWithHexString:@"#cccccc"];
+    self.searchBar.textFieldBackgroundColor = [UIColor colorWithHexString:@"#e6e6ed"];
+    self.searchBar.iconAlign = LZSearchBarIconAlignCenter;
+    [self.view addSubview:self.searchBar];
     
     //设置顶部时间筛选
     _headView = [[UIView alloc]init];
     _headView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:_headView];
     [_headView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.view).offset(LLNavViewHeight);
+//        make.top.equalTo(self.view).offset(LLNavViewHeight);
+        make.top.equalTo(self.searchBar.mas_bottom);
         make.left.and.right.equalTo(self.view);
         make.height.mas_offset(39);
     }];
@@ -68,11 +98,11 @@
     _dateLbl = [[UILabel alloc]init];
     _dateLbl.textColor = CD_Text66;
     _dateLbl.font = FONT(14);
-    _dateLbl.text = @"2018-04-12";
+    _dateLbl.text = @"全部";
     _dateLbl.textAlignment = NSTextAlignmentRight;
     [_rigthHeadView addSubview:_dateLbl];
     [_dateLbl mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.mas_offset(100);
+        make.width.mas_offset(200);
         make.height.mas_offset(15);
         make.right.equalTo(dateImageView.mas_left).offset(-10);
         make.centerY.equalTo(dateImageView);
@@ -95,7 +125,10 @@
 - (void)setupListData{
     NSDictionary * param = @{@"companyId":[BXSUser currentUser].companyId,
                              @"pageNo":@"1",
-                             @"pageSize":@"15"
+                             @"pageSize":@"15",
+                             @"startDate":_startStr,
+                             @"endDate":_endStr,
+                             @"searchName":self.searchBar.text
                              };
     [BXSHttp requestGETWithAppURL:@"finance/expend_list.do" param:param success:^(id response) {
         LLBaseModel * baseModel = [LLBaseModel LLMJParse:response];
@@ -153,10 +186,86 @@
     
 }
 
-
-#pragma mark ----- 点击事件 ------
-- (void)headerTapClick{
+#pragma mark --- 日历 ---
+//初始化日历
+- (void)setupPageView {
+    CGFloat statusHeight = CGRectGetHeight([UIApplication sharedApplication].statusBarFrame);
+    CGFloat pageTitleViewY = 0;
+    if (statusHeight == 20.0) {
+        pageTitleViewY = 64;
+    } else {
+        pageTitleViewY = 88;
+    }
     
+    NSArray *titleArr = @[@"日历",@"周历",@"月历",@"季度"];
+    SGPageTitleViewConfigure *configure = [SGPageTitleViewConfigure pageTitleViewConfigure];
+    configure.indicatorAdditionalWidth = MAXFLOAT; // 说明：指示器额外增加的宽度，不设置，指示器宽度为标题文字宽度；若设置无限大，则指示器宽度为按钮宽度
+    configure.titleSelectedColor = RGB(59, 177, 239);
+    configure.indicatorColor = RGB(59, 177, 239);;
+    /// pageTitleView
+    self.pageTitleView = [SGPageTitleView pageTitleViewWithFrame:CGRectMake(0, pageTitleViewY, self.view.frame.size.width, 44) delegate:self titleNames:titleArr configure:configure];
+    self.pageTitleView.backgroundColor = [UIColor whiteColor];
+    //    [self.view addSubview:_pageTitleView];
+    
+    LLDayCalendarVc *dayVC = [[LLDayCalendarVc alloc] init];
+    dayVC.delegate = self;
+    LLWeekCalendarVc *weekVC = [[LLWeekCalendarVc alloc] init];
+    LLMonthCalendarVc *monthVC = [[LLMonthCalendarVc alloc] init];
+    LLQuarterCalendarVc *quarterVC = [[LLQuarterCalendarVc alloc] init];
+    
+    NSArray *childArr = @[dayVC, weekVC, monthVC, quarterVC];
+    /// pageContentView
+    //    CGFloat contentViewHeight = APPHeight - CGRectGetMaxY(_pageTitleView.frame);
+    self.pageContentView = [[SGPageContentView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_pageTitleView.frame), APPWidth, 350) parentVC:self childVCs:childArr];
+    _pageContentView.delegatePageContentView = self;
+    //    [self.view addSubview:_pageContentView];
+    
+    
+    _bottomView = [[UIView alloc]initWithFrame:self.view.bounds];
+    _bottomView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.3];
+    _bottomView.hidden = YES;
+    
+    [_bottomView addSubview:_pageTitleView];
+    [_bottomView addSubview:_pageContentView];
+    [self.view addSubview:_bottomView];
+}
+
+- (void)pageTitleView:(SGPageTitleView *)pageTitleView selectedIndex:(NSInteger)selectedIndex {
+    [self.pageContentView setPageContentViewCurrentIndex:selectedIndex];
+}
+
+- (void)pageContentView:(SGPageContentView *)pageContentView progress:(CGFloat)progress originalIndex:(NSInteger)originalIndex targetIndex:(NSInteger)targetIndex {
+    [self.pageTitleView setPageTitleViewWithProgress:progress originalIndex:originalIndex targetIndex:targetIndex];
+}
+
+//点击选择日期按钮
+- (void)headerTapClick{
+    _bottomView.hidden = NO;
+}
+//点击日历确定
+- (void)didaffirmBtnInCalendarWithDateStartStr:(NSString *)StartStr andEndStr:(NSString *)EndStr{
+    _startStr = [BXSTools stringFromTData:StartStr];
+    _endStr = [BXSTools stringFromTData:EndStr];
+    _bottomView.hidden = YES;
+    [self setupListData];
+    if (![_startStr isEqualToString:@"0"]) {
+        _dateLbl.text = [NSString stringWithFormat:@"    %@ 至 %@",_startStr,_endStr];
+    }else{
+        _dateLbl.text = _endStr;
+    }
+    
+}
+
+//点击日历取消
+- (void)didCancelBtnInCalendar{
+    _bottomView.hidden = YES;
+}
+
+- (void)searchBar:(LZSearchBar *)searchBar textDidChange:(NSString *)searchText{
+    if (self.searchBar.text.length < 1) {
+        return;
+    }
+    [self setupListData];
 }
 
 - (void)didReceiveMemoryWarning {
