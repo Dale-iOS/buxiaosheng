838431cee192aff106cc79a18b1861e65c44446a
lanyuzx <lanyuzx@qq.com>
2018-07-16 18:10:43 +0800

    饼图

--- BuXiaoShengTests/Classes/Home(首页)/C/NowDayViewController.m
+++ BuXiaoShengTests/Classes/Home(首页)/C/NowDayViewController.m
@@ -40,7 +40,7 @@
 
 -(void)setupData {
     NSDictionary * param = @{@"companyId":[BXSUser currentUser].companyId,
-                             @"dateType":@2
+                             @"dateType":@4
                              };
     [BXSHttp requestGETWithAppURL:@"data_report/index.do" param:param success:^(id response) {
         LLBaseModel * baseModel = [LLBaseModel LLMJParse:response];
@@ -57,44 +57,46 @@
 }
 
 -(void)setupPieCharData {
-    int count = 4;//饼状图总共有几块组成
     //每个区块的数据
     NSMutableArray *yVals = [[NSMutableArray alloc] init];
-    for (int i = 0; i < count; i++) {
-        switch (0) {//大货列表
-            case 0:
-            {
-                [self.pieChartModel.bigGoodsList enumerateObjectsUsingBlock:^(LLBigGoodsListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                   [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:@"大货列表" icon: nil]];
-                }];
-                break;
-            }
-                break;
-            case 1://销售业绩列表
-            {
-                [self.pieChartModel.saleList enumerateObjectsUsingBlock:^(LLSaleListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                    [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:@"销售业绩列表" icon: nil]];
-                }];
-            }
-                break;
-            case 2://板布列表
-            {
-                [self.pieChartModel.sheetClothList enumerateObjectsUsingBlock:^(LLSheetClothListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                  [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:@"板布列表" icon: nil]];
-                }];
-            }
-                break;
-            case 3://最近半年营业额度列表
-            {
-                [self.pieChartModel.turnoverList enumerateObjectsUsingBlock:^(LLTurnoverListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                    [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.amount label:@"最近半年营业额度列表" icon: nil]];
-                }];
-            }
-                break;
-            default:
-                break;
-        }
-    }
+    
+    [self.pieChartModel.bigGoodsList enumerateObjectsUsingBlock:^(LLBigGoodsListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:obj.productName icon: nil]];
+    }];
+//    for (int i = 0; i < count; i++) {
+//        switch (i) {//大货列表
+//            case 0:
+//            {
+//                [self.pieChartModel.bigGoodsList enumerateObjectsUsingBlock:^(LLBigGoodsListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                   [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:@"大货列表" icon: nil]];
+//                }];
+//            }
+//                break;
+//            case 1://销售业绩列表
+//            {
+//                [self.pieChartModel.saleList enumerateObjectsUsingBlock:^(LLSaleListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                    [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:@"销售业绩列表" icon: nil]];
+//                }];
+//            }
+//                break;
+//            case 2://板布列表
+//            {
+//                [self.pieChartModel.sheetClothList enumerateObjectsUsingBlock:^(LLSheetClothListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                  [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.number label:@"板布列表" icon: nil]];
+//                }];
+//            }
+//                break;
+//            case 3://最近半年营业额度列表
+//            {
+//                [self.pieChartModel.turnoverList enumerateObjectsUsingBlock:^(LLTurnoverListModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                    [yVals addObject: [[PieChartDataEntry alloc] initWithValue:obj.amount label:@"最近半年营业额度列表" icon: nil]];
+//                }];
+//            }
+//                break;
+//            default:
+//                break;
+//        }
+//    }
         PieChartDataSet *dataSet = [[PieChartDataSet alloc] initWithValues:yVals label:@"Election Results"];
         
         dataSet.drawIconsEnabled = NO;
@@ -106,11 +108,12 @@
         
         NSMutableArray *colors = [[NSMutableArray alloc] init];
         [colors addObjectsFromArray:ChartColorTemplates.material];
-//        [colors addObjectsFromArray:ChartColorTemplates.joyful];
-//        [colors addObjectsFromArray:ChartColorTemplates.colorful];
-//        [colors addObjectsFromArray:ChartColorTemplates.liberty];
-//        [colors addObjectsFromArray:ChartColorTemplates.pastel];
-//        [colors addObject:[UIColor colorWithRed:51/255.f green:181/255.f blue:229/255.f alpha:1.f]];
+      [colors addObjectsFromArray:ChartColorTemplates.vordiplom];
+      [colors addObjectsFromArray:ChartColorTemplates.joyful];
+      [colors addObjectsFromArray:ChartColorTemplates.colorful];
+      [colors addObjectsFromArray:ChartColorTemplates.liberty];
+      [colors addObjectsFromArray:ChartColorTemplates.pastel];
+      [colors addObject:[UIColor colorWithRed:51/255.f green:181/255.f blue:229/255.f alpha:1.f]];
     
         dataSet.colors = colors;
         
@@ -126,6 +129,9 @@
         [data setValueTextColor:UIColor.whiteColor];
         _pieChartView.data = data;
         [_pieChartView highlightValues:nil];
+    
+     NSMutableAttributedString *centerText = [[NSMutableAttributedString alloc] initWithString:@"大货列表"];
+    _pieChartView.centerAttributedText = centerText;
   
 }
 
@@ -147,11 +153,11 @@
         [_pieChartView setExtraOffsetsWithLeft:30 top:0 right:30 bottom:0];//饼状图距离边缘的间隙
         _pieChartView.usePercentValuesEnabled = YES;//是否根据所提供的数据, 将显示数据转换为百分比格式
         _pieChartView.dragDecelerationEnabled = YES;//拖拽饼状图后是否有惯性效果
-        _pieChartView.drawCenterTextEnabled = false;//是否显示区块文本
-        _pieChartView.drawHoleEnabled = false;//饼状图是否是空心
-        _pieChartView.holeRadiusPercent = 0.1;//空心半径占比
+        _pieChartView.drawCenterTextEnabled = true;//是否显示区块文本
+        _pieChartView.drawHoleEnabled = true;//饼状图是否是空心
+        _pieChartView.holeRadiusPercent = 0.6;//空心半径占比
         _pieChartView.holeColor = [UIColor clearColor];//空心颜色
-        _pieChartView.transparentCircleRadiusPercent = 0.0;//半透明空心半径占比
+        _pieChartView.transparentCircleRadiusPercent = 0.4;//半透明空心半径占比
         _pieChartView.transparentCircleColor = [UIColor clearColor];//半透明空心的颜色
        
         //添加图例
